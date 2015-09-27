1c4f71bc52d4f61b9d740173966b48014f833d5c
Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>
2015-09-27 15:26:20 +0300

    RE finished
    Signed-off-by: Ivaylo Dimitrov <ivo.g.dimitrov.75@gmail.com>

--- src/jabber-plugin.c
+++ src/jabber-plugin.c
@@ -7,8 +7,11 @@
 #include <librtcom-accounts-widgets/rtcom-dialog-context.h>
 #include <librtcom-accounts-widgets/rtcom-login.h>
 #include <librtcom-accounts-widgets/rtcom-edit.h>
+#include <librtcom-accounts-widgets/rtcom-param-bool.h>
 #include <librtcom-accounts-widgets/rtcom-param-int.h>
+#include <librtcom-accounts-widgets/rtcom-param-string.h>
 #include <hildon-uri.h>
+#include <telepathy-glib/util.h>
 #include <config.h>
 
 struct _JabberPlugin {
@@ -22,13 +25,17 @@ struct _JabberPluginClass {
 typedef struct _JabberPluginClass JabberPluginClass;
 
 struct _JabberPluginPrivate {
-  int unk;
+  GtkWidget *dialog;
 };
 typedef struct _JabberPluginPrivate JabberPluginPrivate;
 
 RtcomAccountPluginClass *parent_class;
 
 ACCOUNT_DEFINE_PLUGIN(JabberPlugin, jabber_plugin, RTCOM_TYPE_ACCOUNT_PLUGIN);
+
+#define JABBER_PLUGIN_TYPE jabber_plugin_get_type()
+#define JABBER_PLUGIN_GET_PRIVATE(o) \
+    (G_TYPE_INSTANCE_GET_PRIVATE((o), JABBER_PLUGIN_TYPE, JabberPluginPrivate))
 
 static void jabber_plugin_init(JabberPlugin *self)
 {
@@ -163,35 +170,35 @@ static void get_advanced_settings (GtkWidget *widget,
 
   name = gtk_widget_get_name (widget);
 
-  if (RTCOM_IS_PARAM_INT (widget))
+  if (RTCOM_IS_PARAM_INT(widget))
     {
-      gint value = rtcom_param_int_get_value (RTCOM_PARAM_INT (widget));
-      g_hash_table_replace (advanced_settings, widget,
-          GINT_TO_POINTER (value));
+      gint value = rtcom_param_int_get_value (RTCOM_PARAM_INT(widget));
+      g_hash_table_replace(advanced_settings, widget,
+          GINT_TO_POINTER(value));
     }
   else if (GTK_IS_ENTRY (widget))
     {
-      gchar *data = g_strdup (gtk_entry_get_text (GTK_ENTRY (widget)));
-      g_object_set_data_full (G_OBJECT (widget), "adv_data", data, g_free);
-      g_hash_table_replace (advanced_settings, widget, data);
+      gchar *data = g_strdup(gtk_entry_get_text (GTK_ENTRY (widget)));
+      g_object_set_data_full(G_OBJECT (widget), "adv_data", data, g_free);
+      g_hash_table_replace(advanced_settings, widget, data);
     }
-  else if (HILDON_IS_CHECK_BUTTON (widget))
+  else if (HILDON_IS_CHECK_BUTTON(widget))
     {
       gboolean active =
-        hildon_check_button_get_active (HILDON_CHECK_BUTTON (widget));
-      g_hash_table_replace (advanced_settings, widget,
-          GINT_TO_POINTER (active));
+        hildon_check_button_get_active(HILDON_CHECK_BUTTON (widget));
+      g_hash_table_replace(advanced_settings, widget,
+          GINT_TO_POINTER(active));
     }
-  else if (GTK_IS_CONTAINER (widget))
+  else if (GTK_IS_CONTAINER(widget))
     {
-      gtk_container_foreach (GTK_CONTAINER (widget),
+      gtk_container_foreach(GTK_CONTAINER (widget),
           (GtkCallback)get_advanced_settings,
           advanced_settings);
     }
-  else if (!GTK_IS_LABEL (widget))
+  else if (!GTK_IS_LABEL(widget))
     {
       g_warning ("%s: unhandled widget type %s (%s)", G_STRLOC,
-          g_type_name (G_TYPE_FROM_INSTANCE (widget)), name);
+          g_type_name(G_TYPE_FROM_INSTANCE(widget)), name);
     }
 }
 
@@ -252,7 +259,8 @@ static GtkWidget *create_advanced_settings_page(RtcomDialogContext *context)
     AccountItem *account;
     GtkWidget *button;
     GHashTable *settings;
-    char title[200];
+    gchar *title;
+    const gchar *format;
 
     GladeXML *xml = glade_xml_new(PLUGIN_XML_DIR "/jabber-advanced.glade",
                                   NULL,
@@ -266,7 +274,7 @@ static GtkWidget *create_advanced_settings_page(RtcomDialogContext *context)
     }
 
     gtk_dialog_add_buttons(GTK_DIALOG(dialog),
-                           dgettext("hildon-libs", "wdgt_bd_done"),
+                           g_dgettext("hildon-libs", "wdgt_bd_done"),
                            GTK_RESPONSE_OK,
                            NULL);
     g_object_ref(dialog);
@@ -279,6 +287,7 @@ static GtkWidget *create_advanced_settings_page(RtcomDialogContext *context)
     glade_xml_signal_connect(xml,
                              "on_require_encryption_toggled",
                              G_CALLBACK(on_require_encryption_toggled));
+
     on_require_encryption_toggled(button);
 
     button = glade_xml_get_widget(xml, "force-old-ssl-Button-finger");
@@ -292,15 +301,15 @@ static GtkWidget *create_advanced_settings_page(RtcomDialogContext *context)
 
     account = account_edit_context_get_account(ACCOUNT_EDIT_CONTEXT(context));
     rtcom_page_set_account(RTCOM_PAGE(glade_xml_get_widget(xml, "page")),
-                           RTCOM_ACCOUNT_ITEM (account));
-
-    g_snprintf(title, sizeof(title),
-               g_dgettext("osso-applet-accounts",
-                          "accountwizard_ti_advanced_settings"),
-               account_service_get_display_name(
-                 account_item_get_service(account)));
-
+                           RTCOM_ACCOUNT_ITEM(account));
+    format = g_dgettext("osso-applet-accounts",
+                        "accountwizard_ti_advanced_settings");
+    title = g_strdup_printf(format, account_service_get_display_name(
+                              account_item_get_service(account)));
     gtk_window_set_title(GTK_WINDOW(dialog), title);
+
+    g_free(title);
+
     gtk_window_set_transient_for(GTK_WINDOW(dialog),
                                  GTK_WINDOW(_get_toplevel_widget(context)));
     gtk_window_set_destroy_with_parent(GTK_WINDOW(dialog), TRUE);
@@ -340,9 +349,244 @@ static void jabber_plugin_on_advanced_cb(gpointer data)
   gtk_widget_show(dialog);
 }
 
+static void settings_for_each(gpointer key, gpointer value, gpointer user_data)
+{
+  GHashTable *hash_table = (GHashTable *)user_data;
+
+  if (RTCOM_IS_PARAM_STRING(key) && value && *((char *) value))
+  {
+    g_hash_table_insert(hash_table,
+                        (gpointer)rtcom_param_string_get_field(key),
+                        tp_g_value_slice_new_static_string(
+                          (const gchar *)value));
+  }
+  else if (RTCOM_IS_PARAM_BOOL(key))
+  {
+    g_hash_table_insert(hash_table,
+                        (gpointer)rtcom_param_bool_get_field(key),
+                        tp_g_value_slice_new_boolean(GPOINTER_TO_INT(value)));
+  }
+  else if (RTCOM_IS_PARAM_INT(key))
+  {
+    g_hash_table_insert(hash_table,
+                        (gpointer)rtcom_param_int_get_field(key),
+                        tp_g_value_slice_new_uint(GPOINTER_TO_INT(value)));
+  }
+}
+
+static void connect_cb(GObject *obj, TpConnection *conn, GError *error,
+                       gpointer user_data)
+{
+  JabberPluginPrivate *priv =
+      JABBER_PLUGIN_GET_PRIVATE(account_edit_context_get_plugin(
+                                  ACCOUNT_EDIT_CONTEXT(obj)));
+  (void)conn;
+
+  if (priv->dialog) {
+    gtk_widget_destroy(priv->dialog);
+    priv->dialog = NULL;
+  }
+
+  if (error)
+    hildon_banner_show_information(GTK_WIDGET(user_data), NULL, error->message);
+  else {
+    rtcom_dialog_context_set_start_page(RTCOM_DIALOG_CONTEXT(obj), NULL);
+    gtk_dialog_response(
+          GTK_DIALOG(_get_toplevel_widget(RTCOM_DIALOG_CONTEXT(obj))),
+          1);
+  }
+}
+
+static void jabber_plugin_new_account(GtkWidget *widget, gint response,
+                                      AccountEditContext *ctx)
+{
+  GladeXML *tree;
+  GtkWidget *page;
+  GHashTable *settings;
+  GtkWidget *username;
+  GtkWidget *password;
+  AccountItem *account;
+  gchar *title;
+  const gchar *format;
+  GtkWidget *dialog;
+  GHashTable *hash_table;
+  GError *error = NULL;
+
+  if (response != GTK_RESPONSE_OK) {
+    gtk_widget_destroy(widget);
+
+    return;
+  }
+
+  tree = glade_get_widget_tree(widget);
+  page = glade_xml_get_widget(tree, "page");
+
+  if (!rtcom_page_validate((RtcomPage *)page, &error))
+    goto out;
+
+  if (!g_strstr_len(
+        gtk_entry_get_text(GTK_ENTRY(glade_xml_get_widget(tree, "username"))),
+        -1, "@")) {
+    g_set_error(&error,
+                g_quark_from_static_string("account-error-quark"),
+                4,
+                g_dgettext("osso-applet-accounts",
+                           "accountwizard_ib_illegal_server_address"));
+    goto out;
+  }
+
+  if (strcmp(gtk_entry_get_text(
+               GTK_ENTRY(glade_xml_get_widget(tree, "password"))),
+             gtk_entry_get_text(
+               GTK_ENTRY(glade_xml_get_widget(tree, "password2"))))) {
+    g_set_error(&error,
+                g_quark_from_static_string("account-error-quark"),
+                6, g_dgettext("osso-applet-accounts",
+                              "accountwizard_ib_passwords_do_not_match"));
+    goto out;
+  }
+
+  hash_table = g_hash_table_new_full(g_str_hash, g_str_equal, 0,
+                                     (GDestroyNotify )tp_g_value_slice_free);
+
+  settings = (GHashTable *)g_object_get_data(G_OBJECT(ctx), "settings");
+
+  if (settings)
+    g_hash_table_foreach(settings, settings_for_each, hash_table);
+
+  username = glade_xml_get_widget(tree, "username");
+  password = glade_xml_get_widget(tree, "password");
+
+  g_hash_table_insert(hash_table,
+                      "register",
+                      tp_g_value_slice_new_boolean(TRUE));
+
+  g_hash_table_insert(hash_table,
+                      "account",
+                      tp_g_value_slice_new_static_string(
+                        gtk_entry_get_text(GTK_ENTRY(username))));
+
+  g_hash_table_insert(hash_table,
+                      "password",
+                      tp_g_value_slice_new_static_string(
+                        gtk_entry_get_text(GTK_ENTRY(password))));
+
+
+  account = account_edit_context_get_account(ctx);
+
+  rtcom_account_service_connect(
+        RTCOM_ACCOUNT_SERVICE(account_item_get_service(account)),
+        hash_table,
+        &ctx->parent_instance,
+        TRUE,
+        (RtcomAccountServiceConnectionCb)connect_cb,
+        widget);
+
+  g_hash_table_unref(hash_table);
+
+  format = g_dgettext("osso-applet-accounts", "accounts_ti_registering");
+  title = g_strdup_printf(format, gtk_entry_get_text(GTK_ENTRY(username)));
+  dialog = gtk_dialog_new();
+  gtk_window_set_title(GTK_WINDOW(dialog), title);
+
+  g_free(title);
+
+  gtk_dialog_set_has_separator(GTK_DIALOG(dialog), FALSE);
+  hildon_gtk_window_set_progress_indicator(GTK_WINDOW(dialog), TRUE);
+
+  if (account->service_icon)
+    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox),
+                       gtk_image_new_from_pixbuf(account->service_icon),
+                       0, 0, 16);
+  if (widget) {
+    gtk_window_set_transient_for(GTK_WINDOW(dialog), GTK_WINDOW((widget)));
+    gtk_window_set_modal(GTK_WINDOW(dialog), FALSE);
+  }
+
+  JABBER_PLUGIN_GET_PRIVATE(
+        account_edit_context_get_plugin(ctx))->dialog = dialog;
+
+  gtk_widget_show_all(dialog);
+
+out:
+    if (error) {
+      hildon_banner_show_information(widget, NULL, error->message);
+      g_error_free(error);
+    }
+}
+
 static void jabber_plugin_on_register_cb(gpointer userdata)
 {
+  AccountEditContext *ctx = (AccountEditContext *)userdata;
+  gchar *registration_url;
 
+#if 0
+  /* No idea why it is needed, not to say that noone g_unref's it */
+  GObject *plugin;
+  g_object_get(userdata, "plugin", &plugin, 0);
+#endif
+
+  registration_url =
+      _get_string_from_profile(ctx, "Profile", "RegistrationUrl");
+
+  if (registration_url) {
+    GError *error = 0;
+    if (!hildon_uri_open(registration_url, NULL, &error)) {
+      g_warning("Failed to open browser: %s", error->message);
+      g_error_free(error);
+    }
+
+    g_free(registration_url);
+  } else {
+    GladeXML *xml = glade_xml_new(PLUGIN_XML_DIR "jabber-new-account.glade",
+                                  NULL,
+                                  GETTEXT_PACKAGE);
+    GtkWidget *register_dialog = glade_xml_get_widget(xml, "register");
+    GtkWidget *advanced_button;
+
+    rtcom_dialog_context_take_obj(RTCOM_DIALOG_CONTEXT(ctx), &xml->parent);
+
+    if (!register_dialog) {
+      g_warning("Unable to load Register dialog");
+
+      return;
+    }
+
+    advanced_button = glade_xml_get_widget(xml, "advanced-button");
+
+    g_signal_connect_data(
+          advanced_button, "clicked",
+          G_CALLBACK(jabber_plugin_on_advanced_cb), ctx, NULL,
+          G_CONNECT_SWAPPED);
+
+    gtk_dialog_add_buttons(GTK_DIALOG(register_dialog),
+                           g_dgettext("osso-applet-accounts",
+                                      "accounts_bd_register"),
+                           GTK_RESPONSE_OK,
+                           NULL);
+
+    rtcom_page_set_account(
+          RTCOM_PAGE(glade_xml_get_widget(xml, "page")),
+          RTCOM_ACCOUNT_ITEM(account_edit_context_get_account(ctx)));
+
+    gtk_window_set_title(GTK_WINDOW(register_dialog),
+                         g_dgettext("osso-applet-accounts",
+                                    "accounts_ti_new_jabber_account"));
+
+    gtk_window_set_transient_for(
+          GTK_WINDOW(register_dialog),
+          GTK_WINDOW(_get_toplevel_widget(RTCOM_DIALOG_CONTEXT(ctx))));
+
+    g_signal_connect_data(
+          register_dialog,
+          "response",
+          G_CALLBACK(jabber_plugin_new_account),
+          ctx,
+          NULL,
+          0);
+
+    gtk_widget_show_all(register_dialog);
+  }
 }
 
 static void jabber_plugin_class_context_init(RtcomAccountPlugin *plugin,
@@ -382,9 +626,7 @@ static void jabber_plugin_class_context_init(RtcomAccountPlugin *plugin,
     rtcom_edit_connect_on_advanced(RTCOM_EDIT(page),
                                    G_CALLBACK(jabber_plugin_on_advanced_cb),
                                    context);
-  }
-  else
-  {
+  } else {
      gchar *username_prefill =
          _get_string_from_profile(ACCOUNT_EDIT_CONTEXT(context),
                                   "Profile", "Prefill");
